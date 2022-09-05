da88cf39e95c6548bbeb6a358553ddeebcc2e7ae
develiberta <develiberta@gmail.com>
2022-09-05 18:03:59 +0900

    Animation (Speed에 따른) 추가 중

---
+++ Source/TestUnrealEngine/MyAnimInstance.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "MyAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTUNREALENGINE_API UMyAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+private:
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Pawn, Meta=(AllowPrivateAccess=true))
+	float Speed;
+};
